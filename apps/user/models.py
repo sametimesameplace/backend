87b62394da66883591e87436e5d5512216ba2078
Jaime <jay4000@hotmail.co.uk>
2023-11-02 11:05:10 +0100

    Created the user model

--- apps/user/models.py
+++ apps/user/models.py
@@ -3,6 +3,14 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
+
 class User(AbstractUser):
-    title = models.CharField(max_length=100, null=True, blank=True, help_text="Your profession")
-    bio = models.TextField(default='', blank=True)
+    title = models.CharField(
+        max_length=100, null=True, blank=True, help_text="Your profession"
+    )
+    bio = models.TextField(default="", blank=True)
+    username = (models.CharField(max_length=100),)
+    password = (models.CharField(max_length=100),)
+    email = (models.CharField(max_length=100),)
+    mfa_secret = (models.CharField(max_length=100),)
+    date_joined = models.DateTimeField(auto_now_add=True)
