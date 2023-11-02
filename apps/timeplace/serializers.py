9d10d8d96ed5f91c675e247ec32121fa9a417c10
Termuellinator <saldorin@web.de>
2023-11-02 16:15:54 +0100

    Add Serializer for TimePlace model

--- apps/timeplace/serializers.py
+++ apps/timeplace/serializers.py
@@ -1,5 +1,7 @@
+from datetime import datetime
 from rest_framework import serializers
 
+from apps.user.serializers import UserModelSerializer
 from . import models
 
 
@@ -13,4 +15,38 @@ class ActivityModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Activity
         fields = "__all__"
-        
+
+
+class TimePlaceModelSerializer(serializers.ModelSerializer):
+    user_id = UserModelSerializer()
+    interests = InterestModelSerializer(many=True)
+    activities = ActivityModelSerializer(many=True)
+    
+    class Meta:
+        model = models.TimePlace
+        fields = [
+            "id",
+            "user_id",
+            "start",
+            "end",
+            "latitude",
+            "longitude",
+            "description",
+            "interests",
+            "activities"
+        ]
+                  
+    def validate(self, data):
+        if data['start'] < datetime.now():
+            raise serializers.ValidationError(
+                "Start date has to be in the future.")
+        if data["end"] <= data["start"]:
+            raise serializers.ValidationError(
+                "End date has to be after start date.")
+        if data["latitude"] > 90 or data["latitude"] < -90:
+            raise serializers.ValidationError(
+                "Latitude has to be between +90° and -90°")
+        if data["longitude"] > 180 or data["longitude"] < -180:
+            raise serializers.ValidationError(
+                "Longitude has to be between +180° and -180°")
+        return data
