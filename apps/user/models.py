a5bf1d5d17cd98d63a0dc25fcac5c16b6edd017b
Jaime <jay4000@hotmail.co.uk>
2023-11-02 13:23:00 +0100

    Modified the user model after the review

--- apps/user/models.py
+++ apps/user/models.py
@@ -9,8 +9,3 @@ class User(AbstractUser):
         max_length=100, null=True, blank=True, help_text="Your profession"
     )
     bio = models.TextField(default="", blank=True)
-    username = (models.CharField(max_length=100),)
-    password = (models.CharField(max_length=100),)
-    email = (models.CharField(max_length=100),)
-    mfa_secret = (models.CharField(max_length=100),)
-    date_joined = models.DateTimeField(auto_now_add=True)
