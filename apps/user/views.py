61a0d1ff7260febe73f61ba3dfa36383a28a528e
Swetlana-K <swetlana.koenig@gmx.de>
2023-11-03 16:09:37 +0100

    add api-view for login

--- apps/user/views.py
+++ apps/user/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from typing import Optional
 
-# Create your views here.
+from django.contrib.auth import authenticate
+
+from rest_framework import status  
+from rest_framework.views import APIView  
+from rest_framework.response import Response  
+from rest_framework.authtoken.models import Token 
+
+from .models import User
+
+
+class UserLoginView(APIView):
+    def post(self, request):
+        # get credentials
+        password = request.data.get('password')
+        username = request.data.get('username')
+
+        # authenticate the user
+        user: Optional[User] = authenticate(
+            username = username,
+            password = password,
+        )
+        if not user:
+            return Response(
+                {"error": "Password or Username incorrect"},
+                status=status.HTTP_403_FORBIDDEN
+            )
+
+        # Create the token
+        token, _ = Token.objects.get_or_create(user=user)
+
+        return Response(
+            {"token": token.key},
+            status=status.HTTP_200_OK
+        )
