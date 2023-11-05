32d9eebe8d2d9e560ff5c15ee3c578d205bd2a7c
Jaime <jay4000@hotmail.co.uk>
2023-11-05 15:50:01 +0100

    Modified the user serializer to overwrite the create method as the password was not being hashed in the database

--- apps/user/serializers.py
+++ apps/user/serializers.py
@@ -4,6 +4,22 @@ from . import models
 
 
 class UserModelSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    def create(self, validated_data):
+        user = models.User.objects.create_user(
+            username=validated_data["username"],
+            password=validated_data["password"],
+            email=validated_data["email"],
+        )
+
+        return user
+
     class Meta:
         model = models.User
-        fields = "__all__"
+        fields = (
+            "username",
+            "password",
+            "email",
+            "date_joined",
+        )
