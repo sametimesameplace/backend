8804456e500b5335bfcb4ca480769f13672643dd
Jaime <jay4000@hotmail.co.uk>
2023-11-08 15:39:56 +0100

    Adds a test for user updating their data

--- apps/user/tests/ut/test_endpoints.py
+++ apps/user/tests/ut/test_endpoints.py
@@ -23,8 +23,15 @@ class TestUserEndpoints(APITestCase):
         )
         cls.supertoken = Token.objects.create(user=cls.superuser)
 
+        cls.user = User.objects.create_user(
+            username="test_user",
+            email="test_user@stsp.com",
+            password="test_user_2023",
+        )
+        cls.usertoken = Token.objects.create(user=cls.user)
+
     def test_user_list_authorized(self):
-        """Tests that a list of users is returned for the admin only"""
+        """Tests that a list of users are returned for admin only"""
 
         url = reverse("user-list")
         response = self.client.get(url)
@@ -48,13 +55,13 @@ class TestUserEndpoints(APITestCase):
     def test_delete_user(self):
         """Tests that a user can only be deleted by the superuser"""
 
-        self.user = User.objects.create_user(
-            username="user",
-            email="user@stsp.com",
-            password="user_2023",
+        self.user_for_deletion = User.objects.create_user(
+            username="user1",
+            email="user1@stsp.com",
+            password="user1_2023",
         )
 
-        self.usertoken = Token.objects.create(user=self.user)
+        self.usertoken_for_deletion = Token.objects.create(user=self.user_for_deletion)
 
         url = reverse("user-detail", args=(self.user.id,))
         response = self.client.delete(url)
@@ -64,3 +71,25 @@ class TestUserEndpoints(APITestCase):
         self.client.credentials(HTTP_AUTHORIZATION="Token " + self.supertoken.key)
         response = self.client.delete(url)
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_update_user(self):
+        """Tests that a user can update their own data"""
+        self.user_for_updating = User.objects.create_user(
+            username="user2",
+            email="user2@stsp.com",
+            password="user2_2023",
+        )
+
+        self.usertoken_for_updating = Token.objects.create(user=self.user_for_updating)
+
+        new_data = {
+            "username": "bob",
+            "email": "bob@stsp.com",
+            "password": "bob_2023",
+        }
+        url = reverse("user-detail", args=(self.user_for_updating.id,))
+        self.client.credentials(
+            HTTP_AUTHORIZATION="Token " + self.usertoken_for_updating.key
+        )
+        response = self.client.put(url, data=new_data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
