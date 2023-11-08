e65896b6ab12dc74878cd226f7b09e86395b0365
Steffen P <steffen.pretzel@gmx.de>
2023-11-08 11:52:25 +0100

    Adds serializers for the userprofile - Create, Update, Retrieve

--- apps/user/serializers.py
+++ apps/user/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 
 from . import models
 
+from .models import UserProfile
+
 
 class UserModelSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
@@ -15,6 +17,12 @@ class UserModelSerializer(serializers.ModelSerializer):
 
         return user
 
+    def validate_phone(self, value):
+        pass
+
+    def validate_email(self, value):
+        pass
+
     class Meta:
         model = models.User
         fields = (
@@ -23,4 +31,59 @@ class UserModelSerializer(serializers.ModelSerializer):
             "password",
             "email",
             "date_joined",
+        )
+
+
+class UserProfileCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = (
+            "user",
+            "name",
+            "hometown",
+            "slogan",
+            "birthday",
+            "gender",
+            "phone",
+            "profile_email",
+            "languages"
+        )
+
+        def validate_birthday(self, birthday):
+            # Check if the birthday is in the past or age < 18
+            if birthday > timezone.now().date() or :
+                raise serializers.ValidationError("Invalid birthday")
+            return birthday
+
+
+class UserProfileUpdateSerializer(serializers.ModelSerializer):
+    # Birthday cannot be updated
+    class Meta:
+        model = UserProfile
+        fields = (
+            "user",
+            "name",
+            "hometown",
+            "slogan",
+            "gender",
+            "phone",
+            "profile_email",
+            "languages"
+        )
+
+
+class UserProfileRetrieveSerializer(serializers.ModelSerializer):
+    # Returns age instead of birthday
+    class Meta:
+        model = UserProfile
+        fields = (
+            "user",
+            "name",
+            "hometown",
+            "slogan",
+            "age",
+            "gender",
+            "phone",
+            "profile_email",
+            "languages"
         )
