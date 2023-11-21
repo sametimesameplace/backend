8f764f9bca160f34ba34f21ef41e66e81bf78e9c
Jaime <jay4000@hotmail.co.uk>
2023-11-21 10:09:31 +0100

    Modified the match serializers, views, permissions

--- apps/match/views.py
+++ apps/match/views.py
@@ -1,19 +1,48 @@
+from datetime import datetime, timezone
+
 from django.shortcuts import render
+from django.db.models import Q
 
 from rest_framework import status
 from rest_framework import viewsets
+from rest_framework import mixins
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
-from . import models, serializers
+from . import models, serializers, permissions
 
 
-class MatchViewSet(viewsets.ModelViewSet):
+class MatchViewSet(
+                mixins.RetrieveModelMixin,
+                mixins.DestroyModelMixin,
+                mixins.ListModelMixin,
+                viewsets.GenericViewSet):
+    """ 
+    API endpoint that allows users to be viewed or edited.
+    """
     queryset = models.Match.objects.all()
-    permission_classes = [IsAuthenticated]
-    serializer_class = serializers.MatchModelSerializer
+    permission_classes = [permissions.SuperOrAuthors]
+    # serializer = UploadFilesSerializer(data=request.data, context={'request': request})
+    serializer_class = serializers.MatchModelListRetrieveSerializer
 
+    def get_queryset(self):
+        """Limit the queryset to the author, i.e the logged in user, 
+        and non-deleted items for fetching/updating data.
+        Superusers can see all items.
+        """
+        if self.request.user.is_superuser:
+            return models.Match.objects.all()
+        return models.Match.objects.filter(Q(timeplace_1__user=self.request.user) | Q(timeplace_2__user=self.request.user))
+    
+    def perform_destroy(self, instance):
+        """Don't delete the instance but rather set 'deleted' to true
+        and deleted_on to the current timestamp.
+        """
+        instance.deleted = True
+        instance.deleted_on = datetime.now(tz=timezone.utc)
+        instance.save()
+    
     @action(detail=True, methods=["GET"], url_path="chat")
     def chat(self, request, *args, **kwargs):
         match = models.MatchChat.objects.filter(match_id=self.get_object().id)
